33b4a213be0d275031ff81a7ad25a086e183739a
standard-12 <shreenathshenoy12@gmail.com>
2025-10-18 18:30:03 +0530

    Configured Webpack

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,17 +1,38 @@
 // webpack.config.js
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
+//   context: path.resolve(__dirname, '../frontend'),
   entry: "./../frontend/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../frontend/dist"),
     clean: true,
+  },
+    devtool: "eval-source-map",
+   devServer: {
+    watchFiles: ["./src/template.html"],
   },
    plugins: [
     new HtmlWebpackPlugin({
       template: "./../frontend/template.html",
     }),
   ],
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
